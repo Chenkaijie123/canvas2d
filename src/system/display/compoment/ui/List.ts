814993a31546d161bc4a5869930e55b265fa40f8
e <1721225745@qq.com>
2021-03-24 11:07:10 +0800

    list组件提交

--- src/system/display/compoment/ui/List.ts
+++ src/system/display/compoment/ui/List.ts
@@ -4,10 +4,33 @@ import DisplayUtil from "../../math/DisplayUtil";
 import Box from "./Box";
 import Scroller from "./Scroller";
 
+/**
+ * 列表组件
+ *       let list = new List;
+ * 设置渲染列表项
+ *       list.itemRender = ScItem
+ * 设置列表大小
+ *       list.width = 100
+ *       list.height = 100
+ *       list.x = 200
+ *       list.y = 300
+ * 设置滚动方向 1垂直2水平，默认垂直
+ *       list.dirction = 2
+ * 设置列表项渲染函数，列表项被渲染都会调用一次
+ *       list.renderHandle = (item:ScItem,data:string) => {
+ *           item.lab.text = data;
+ *       }
+ * 设置数据源
+ *       list.data = datas
+ * 添加到舞台
+ *       GlobalMgr.stage.addChild(list)
+ * 
+ */
 export default class List<T> extends Scroller {
+    /**垂直间隙 */
     paddingTop: number = 0;
-    paddingLeft:number = 0;
-    itemNum:number = 5;
+    /**水平间隙 */
+    paddingLeft: number = 0;
     /**1垂直滚动 2水平滚动 */
     dirction: 1 | 2 = 1;
     private _itemRender: new () => Box;
@@ -15,9 +38,13 @@ export default class List<T> extends Scroller {
     private listItems: Box[] = [];
     private listItemsPool: Box[] = [];
     private _itemHeight: number = 0;
-    private _itemWidth:number = 0;
+    private _itemWidth: number = 0;
+    /**列表开始显示的数据下标 */
+    private startIndex: number = 0;
+    private container: Box = new Box;
     constructor() {
         super();
+        this.addChild(this.container);
         this.on(SCROLLER_CHANGE, this.onScrollerChange, this);
     }
 
@@ -40,19 +67,26 @@ export default class List<T> extends Scroller {
         this.releaseAllItem();
         let item: DisPlayNode;
         let temp = 0;
-        for (let d of v) {
+        let showLen = this.itemNum + 1;
+        if (v.length < showLen) showLen = v.length;
+        let d: T;
+        for (let idx = 0; idx < showLen; idx++) {
+            d = v[idx];
             item = this.addItem();
-            this.renderHandle && this.renderHandle(item, d);
-            if(this.dirction == 1){
+            this.renderHandle && this.renderHandle(item, d,idx);
+            if (this.dirction == 1) {
                 item.x = 0;
                 item.y = temp;
                 temp += this.itemHeight + this.paddingTop;
-            }else{
+            } else {
                 item.x = temp;
                 item.y = 0;
-                temp += this.itemWidth + this.paddingLeft
+                temp += this.itemWidth + this.paddingLeft;
             }
         }
+        this.startIndex = 0;
+        this.container.width = this.dirction == 1 ? this.width : this.itemWidth * v.length;
+        this.container.height = this.dirction == 1 ? this.itemHeight * v.length : this.height;
     }
 
     get data() {
@@ -61,19 +95,23 @@ export default class List<T> extends Scroller {
 
     private get itemHeight(): number {
         if (this._itemHeight) return this._itemHeight;
-        if (!this.listItems[0]) return 0;
-        this.listItems[0].calcSizeNow();
-        return this._itemHeight = this.listItems[0].height;
+        let temp = new this.itemRender;
+        temp.calcSizeNow();
+        return this._itemHeight = temp.height;
     }
 
-    private get itemWidth():number{
+    private get itemWidth(): number {
         if (this._itemWidth) return this._itemWidth;
-        if (!this.listItems[0]) return 0;
-        this.listItems[0].calcSizeNow();
-        return this._itemWidth = this.listItems[0].width;
+        let temp = new this.itemRender;
+        temp.calcSizeNow();
+        return this._itemWidth = temp.width;
     }
 
-    renderHandle: (v: DisplayUtil, data: T) => void;
+    get itemNum(): number {
+        return this.dirction == 1 ? Math.ceil(this.height / this.itemHeight) : Math.ceil(this.width / this.itemWidth);
+    }
+
+    renderHandle: (v: DisplayUtil, data: T, index: number) => void;
 
     //把列表项放入缓存
     private releaseAllItem(): void {
@@ -87,22 +125,36 @@ export default class List<T> extends Scroller {
     addItem(): DisPlayNode {
         let item = this.listItemsPool.pop() || new this._itemRender();
         this.listItems.push(item);
-        this.addChild(item); 
+        this.container.addChild(item);
         return item;
     }
 
+    /**列表项复用 */
     private onScrollerChange(): void {
-        /**
-         * 1.计算出当前显示的列表项
-         * 2.复用列表项并且赋值刷新
-         */
-        let start :number,end:number;
-        if(this.dirction == 1){
-            start = Math.ceil(Math.abs(this.scrollX) / this.itemHeight) - 1;
-            start < 0 && (start = 0);
-            end = start + this.itemNum - 1;
+        if (this.dirction == 1) {
+            if (this.listItems[0].y < -this.itemHeight - this.scrollY && this.data.length > this.startIndex + this.itemNum) {
+                this.listItems[0].y = this.listItems[this.listItems.length - 1].y + this.itemHeight + this.paddingTop;
+                this.listItems.push(this.listItems[0]);
+                this.renderHandle && this.renderHandle(this.listItems[0], this.data[this.startIndex + this.itemNum + 1], this.startIndex + this.itemNum + 1)
+                this.listItems.shift();
+                this.startIndex++;
+            } else if (this.listItems[0].y > -this.scrollY && this.startIndex > 0) {
+                this.listItems[this.listItems.length - 1].y = this.listItems[0].y - this.itemHeight - this.paddingTop;
+                this.listItems.unshift(this.listItems.pop());
+                this.renderHandle && this.renderHandle(this.listItems[0], this.data[--this.startIndex], this.startIndex);
+            }
+        } else {
+            if (this.listItems[0].x < -this.itemWidth - this.scrollX && this.data.length > this.startIndex + this.itemNum) {
+                this.listItems[0].x = this.listItems[this.listItems.length - 1].x + this.itemWidth + this.paddingLeft;
+                this.listItems.push(this.listItems[0]);
+                this.renderHandle && this.renderHandle(this.listItems[0], this.data[this.startIndex + this.itemNum + 1], this.startIndex + this.itemNum + 1)
+                this.listItems.shift();
+                this.startIndex++;
+            } else if (this.listItems[0].x > -this.scrollX && this.startIndex > 0) {
+                this.listItems[this.listItems.length - 1].x = this.listItems[0].x - this.itemWidth - this.paddingLeft;
+                this.listItems.unshift(this.listItems.pop());
+                this.renderHandle && this.renderHandle(this.listItems[0], this.data[--this.startIndex], this.startIndex);
+            }
         }
-
-        //TODO
     }
 }
--- src/test/ListTest.ts
+++ src/test/ListTest.ts
@@ -4,30 +4,47 @@ import List from "../system/display/compoment/ui/List";
 import GlobalMgr from "../system/global/GlobalMgr";
 import TestBase from "./TestBase";
 
-export default class ListTest extends TestBase{
-    test(){
+export default class ListTest extends TestBase {
+    test() {
         let list = new List;
         list.itemRender = ScItem
         list.width = 100
         list.height = 100
         list.x = 200
         list.y = 100
-        list.renderHandle = (item:ScItem,data:string) => {
+        list.renderHandle = (item: ScItem, data: string, idx: number) => {
             item.lab.text = data;
+            console.log(idx)
         }
         let datas = []
-        for(let i = 0; i < 10;i++){
+        for (let i = 0; i < 10; i++) {
             datas.push(i + "")
         }
         list.data = datas
         GlobalMgr.stage.addChild(list)
+
+        // list = new List;
+        // list.itemRender = ScItem
+        // list.width = 100
+        // list.height = 100
+        // list.x = 200
+        // list.y = 300
+        // list.dirction = 2
+        // list.renderHandle = (item: ScItem, data: string, idx: number) => {
+        //     item.lab.text = data;
+        //     console.log(idx)
+        // }
+        // list.data = datas
+        // GlobalMgr.stage.addChild(list)
     }
 }
 
-class ScItem extends Box{
-    lab:CLabel = new CLabel
-    constructor(){
-        super()
+class ScItem extends Box {
+    lab: CLabel = new CLabel
+    constructor() {
+        super();
+        this.width = 50;
+        this.height = 20
         this.addChild(this.lab);
     }
 }
